ef25a9d6067e4683b5503760f6980ef6640eca41
Samuel Levy <samuellevy@3AWW13.local>
2018-05-30 14:13:37 -0300

    fixing menu

--- src/scenes/components/navigation/index.js
+++ src/scenes/components/navigation/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 import { View, Text, TouchableHighlight } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import SVGImage from 'react-native-svg-image';
 
 import styles from './styles';
 
@@ -12,23 +14,25 @@ export default class Navigation extends Component {
         <View style={styles.container}>
             <View style={styles.navBar}>
                 <View style={styles.tabBtn}>
-                    <Icon name="home" size={25} style={styles.icon} color={"#ddd"}></Icon>
+                    <MaterialIcon name="home" size={25} style={styles.icon} color={"#ddd"}></MaterialIcon>
                     <Text style={styles.title}>Home</Text>
                 </View>
                 <View style={styles.tabBtn}>
-                    <Icon name="school" size={25} style={styles.icon} color={"#ddd"}></Icon>
+                    <MaterialIcon name="school" size={25} style={styles.icon} color={"#ddd"}></MaterialIcon>
                     <Text style={styles.title}>Curso</Text>
                 </View>
                 <View style={styles.tabBtn}>
-                    <Icon name="trending-up" size={25} style={styles.icon} color={"#ddd"}></Icon>
+                <SVGImage style={{ width: 80, height: 80, color: 'red' }} source={{uri:'https://fluent-panda.appspot.com.storage.googleapis.com/dumbbell.svg'}} />
+                    <FontAwesome name="calendar-o" size={25} style={styles.icon} color={"#ddd"}></FontAwesome>
+                    <MaterialIcon name="trending-up" size={15} style={styles.icon} color={"#ddd"}></MaterialIcon>
                     <Text style={styles.title}>Desempenho</Text>
                 </View>
                 <View style={styles.tabBtn}>
-                    <Icon name="star_border" size={25} style={styles.icon} color={"#ddd"}></Icon>
-                    <Text style={styles.title}>Rankin</Text>
+                    <MaterialIcon name="star-border" size={25} style={styles.icon} color={"#ddd"}></MaterialIcon>
+                    <Text style={styles.title}>Ranking</Text>
                 </View>
                 <View style={styles.tabBtn}>
-                    <Icon name="notes" size={25} style={styles.icon} color={"#ddd"}></Icon>
+                    <MaterialIcon name="subject" size={25} style={styles.icon} color={"#ddd"}></MaterialIcon>
                     <Text style={styles.title}>Mais</Text>
                 </View>
             </View>
