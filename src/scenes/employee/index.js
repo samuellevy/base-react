f40675b0b04bc55c5d64221f8ab53d4626259f48
ronald <ronald.junger@3aworldwide.com.br>
2018-06-06 15:10:10 -0300

    funcionarios
    funcionarios

--- src/scenes/employee/index.js
+++ src/scenes/employee/index.js
@@ -7,12 +7,14 @@ import styles from './styles';
 import Pie from  'react-native-pie';
 import TitleTop from '../../components/title/primaryTitle';
 
-
-export default class Home extends Component {
+ 
+export default class Employee extends Component {
 	static navigationOptions = {
-	}	
-		
-	render() {
+		title: 'employee',
+		headerRight:<View style={{flex:1, backgroundColor: 'black', height: 50}}><Text>Funcionarios</Text></View>
+	  };
+		  
+	render() { 
 		return (
 			<View style={styles.container}>
 				<ScrollView style={{marginBottom: 50}}>
