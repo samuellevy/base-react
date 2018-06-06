334a6e752b7535c8f722ade8200fd58536bdfc7f
ronald <ronald.junger@3aworldwide.com.br>
2018-06-06 19:00:50 -0300

    Funcionarios
    Funcionarios

--- src/scenes/employee/addEmployee/styles.js
+++ src/scenes/employee/addEmployee/styles.js
@@ -1,22 +1,43 @@
 import { StyleSheet, Platform } from 'react-native';
-import { colors, metrics, fonts } from '../../styles';
+import { colors, metrics, fonts } from '../../../styles';
 
 const styles = StyleSheet.create({
     container:{
         flex: 1,
         // padding: metrics.containerPadding,
         // paddingTop: (Platform.OS === 'ios') ? metrics.headerPadding : 0,
-        backgroundColor: '#f7fbfc',
-        justifyContent: 'center',
+        backgroundColor: '#f7fbfc', 
         padding: 18,
+    },
+    contentAddUser: {
+        marginTop: 35,
+    },   
+    boxInput: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        borderBottomColor: '#ADCCC7',
+        borderBottomWidth: 2,
+    },
+    inputText: {
+        width: "20%",
+        color: colors.light,
+        fontSize: 10,
+    },
+    input: {
+        width: '80%',
+        borderColor: colors.lighter, 
+        borderBottomWidth: 0,
+        fontSize: 14,
     },
     addEmplayee: {
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 20,
         marginBottom: 10,
+        paddingTop: 50,
     },
     addBtn: {
+        width: "70%", 
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'center',
@@ -25,14 +46,15 @@ const styles = StyleSheet.create({
         backgroundColor: colors.yellow,
         paddingLeft: 20,
         paddingRight: 30,
-        paddingTop: 10,
-        paddingBottom: 10,
+        paddingTop: 20,
+        paddingBottom: 20,
         borderRadius: 50,
         marginBottom: 20,
-    },
+    }, 
     boxIcon: {
         backgroundColor: colors.white,
         borderRadius: 50,
+        width: 15,
     },
     iconClear: { 
         color: colors.yellow,
--- src/scenes/employee/index.js
+++ src/scenes/employee/index.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, ART, Platform } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import NavIcon from '../../components/navigation/NavIcon';
 import styles from './styles';
 
-import Pie from  'react-native-pie';
-import TitleTop from '../../components/title/primaryTitle';
+import Header from '../../components/header';
+import Nav from '../../components/navigation';
 
- 
+import TitleTop from '../../components/title/primaryTitle';
+import ListUser from './components/listEmployee';
+
 export default class Employee extends Component {
-	static navigationOptions = {
-		title: 'employee',
-		headerRight:<View style={{flex:1, backgroundColor: 'black', height: 50}}><Text>Funcionarios</Text></View>
-	  };
-		  
-	render() { 
-		return (
+  	static navigationOptions = {
+    	title: 'products',
+    	headerRight:<View style={{flex:1, backgroundColor: 'black', height: 50}}><Text>HOME</Text></View>
+  	};
+  
+  	render() {
+    	return (
 			<View style={styles.container}>
 				<ScrollView style={{marginBottom: 50}}>
-					<TitleTop textContent={'FUNCIONÁRIOSs'} />
+					<TitleTop textContent={'FUNCIONÁRIOS'} />
+ 
+					<View style={styles.addEmplayee}>
+						<TouchableOpacity style={styles.addBtn} onPress={() => {this.setState({visibleModal: false})} }>
+							<View style={styles.boxIcon}>
+								<MaterialIcon name="add" size={15} style={styles.iconClear}></MaterialIcon>
+							</View>
+							<Text style={styles.textBtn}>ADICIONAR FUNCIONÁRIO</Text>
+						</TouchableOpacity>
+					</View> 
+					 
+					<ListUser icon={true} nameUser="Ronald Junger" mensager="Todos os módulos foram completos!"/>
+					<ListUser icon={false} nameUser="Kayalla Pontes" mensager="Nunca acessou ao curso!"/>
+					<ListUser icon={false} nameUser="Samuel Levy" mensager="Ainda não completou o módulo desse mês."/>
+					<ListUser icon={false} nameUser="Vinicius Machado" mensager="Está atrasado no curso: faltam 2 módulos!"/>
 				</ScrollView>
 			</View>
-		);
-	}
-};
+	    ); 
+  	}
+}
+
+
