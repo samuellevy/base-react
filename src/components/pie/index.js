21dccb5bb4802b6faae13f781e81353bd4334db4
Samuel Levy <samuellevy@3AWW13.local>
2018-06-06 21:55:48 -0300

    animation system for pie component

--- src/components/pie/index.js
+++ src/components/pie/index.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, ART, Platform } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, ART, Platform, Animated, Easing } from 'react-native';
 import styles from './styles';
-
+const timer = require('react-native-timer');
 import Pie from  'react-native-pie';
 
 
 export default class Home extends Component {
-	static navigationOptions = {
-    }
+	state = {
+		percentValue: 0,
+		percent: this.props.percent
+	}
+
+	componentDidMount() {
+		this.setState({percentValue: this.state.percentValue}, () => timer.setInterval(
+            this, 'animatePie', () => 
+            {
+				deltaPercent = this.state.percent / 10;
+                if(this.state.percentValue != this.state.percent){
+                    this.setState({percentValue: this.state.percentValue+deltaPercent})
+                }
+            }
+            , 50
+        ));
+	}
 		
 	render() {
-        let percent = this.props.percent;
+		//let percent = this.props.percent;
 		return ( 
 			<View style={styles.container}>
 				<View>
+					{/* <Animated.View style={[styles.block, {marginLeft} ]} /> */}
 					<Pie
 						radius={50} 
 						innerRadius={35}
-						series={[percent]} 
+						series={[this.state.percentValue]} 
                         colors={['#00A6E3']}
 						backgroundColor='#ddd' />
 					
 					<View style={styles.gauge}>
-						<Text style={styles.gaugeText}>{percent}%</Text>
+						<Text style={styles.gaugeText}>{this.state.percent}%</Text>
 					</View>
 				</View>
 			</View>
