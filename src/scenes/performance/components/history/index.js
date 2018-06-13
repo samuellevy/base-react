c0bed667a3a322a298ae0a7adbd707d8a2166631
Kayalla Pontes <kayalla.p@gmail.com>
2018-06-13 18:46:55 -0300

    ajustes

--- src/scenes/performance/components/history/index.js
+++ src/scenes/performance/components/history/index.js
@@ -8,12 +8,18 @@ import styles from './styles';
 export default class History extends Component {
   render() {
     let date = this.props.date;
+    let description = this.props.description;
+    let score = this.props.score;
 
     return (
       <View style={styles.container}>
-        <Text>
-          {date}
-        </Text>
+        <View style={styles.dateCell}>
+          <Text style={styles.date}>{date}</Text>
+        </View>
+        <View style={styles.introCell}>
+          <Text style={styles.description}>{description}</Text>
+          <Text style={styles.score}>+{score} pts</Text>
+        </View>
       </View>
     );
   }
--- src/scenes/performance/components/history/styles.js
+++ src/scenes/performance/components/history/styles.js
@@ -5,7 +5,16 @@ const styles = StyleSheet.create({
     container: {
         position: 'relative',
         padding: 20,
+        flexDirection: 'row',
     },
+    dateCell:{
+        width: 70,
+    },
+    introCell:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }   
 });
 
 export default styles;
